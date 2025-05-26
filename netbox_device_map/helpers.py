4dccdbc104452a2b6b49e6fd8ca8b8a98884ddbb
Lilian SOULAIROL <104978425+Arcturus34@users.noreply.github.com>
2025-05-26 10:21:02 +0200

    Update helpers.py

--- netbox_device_map/helpers.py
+++ netbox_device_map/helpers.py
@@ -23,25 +23,24 @@ def get_device_location(device: Device) -> LatLon | None:
 
 
 def get_connected_devices(device: Device, vlan: VLAN = None) -> QuerySet[Device]:
-    """Get list of connected devices to the specified device.
-    If the vlan is specified, return only devices connected to the interfaces of the specified device
-    containing the specified VLAN"""
-    included_interfaces = device.interfaces.all()
+    peer_devices = Device.objects.none()
+    interfaces = device.interfaces.exclude(cable=None)
+
     if vlan is not None:
-        included_interfaces = included_interfaces.filter(Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan))
-    if NETBOX_VERSION < version.parse('3.3.0'):
-        return Device.objects.filter(interfaces___link_peer_id__in=included_interfaces)
-    else:
-        return Device.objects.filter(
-            interfaces__cable__terminations__interface__in=device.interfaces.all()
-        ).exclude(pk=device.id)
+        interfaces = interfaces.filter(Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan))
+
+    for interface in interfaces:
+        for connected_endpoint in interface.connected_endpoints:
+            if hasattr(connected_endpoint, 'device'):
+                peer_device = connected_endpoint.device
+                if peer_device.id != device.id:
+                    peer_devices |= Device.objects.filter(id=peer_device.id)
+
+    return peer_devices.distinct()
+
+
+
 
 
 def are_devices_connected(device_a: Device, device_b: Device) -> bool:
-    """Determines whether devices are connected to each other by a direct connection"""
-    if NETBOX_VERSION < version.parse('3.3.0'):
-        queryset = Device.objects.filter(interfaces___link_peer_id__in=device_a.interfaces.all(), id=device_b.id)
-    else:
-        queryset = Device.objects.filter(
-            interfaces__cable__terminations__interface__in=device_a.interfaces.all(), id=device_b.id)
-    return bool(queryset.values('pk'))
+    return get_connected_devices(device_a).filter(id=device_b.id).exists()
